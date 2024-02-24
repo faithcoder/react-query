b654c7ce2a5a09ec4e74dfaf5db3169831c42743
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-02-24 23:50:54 +0600

    product list added

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -15,7 +15,17 @@ export default function ProductList() {
     if(error) return <p>Error: {error.message}</p>;
     return (
         <>
-        <h1>Product List</h1>
+        <div className="flex flex-col justify-center items-center w-3/5">
+            <h2 className="text-3xl my-2"></h2>
+            <ul>
+                {products && products.map((product) => (
+                    <li key={product.id}>
+                        <img src="{product.thumbnail}" alt="{product.title}" />
+                        <p>{product.title}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
         </>
     );
 }
